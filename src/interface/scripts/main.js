34451e83fdd8829ea370aacb3a73acfd6b9cf0f0
Romina Victoria Gonzalez Dreifuss <rominadrei5@gmail.com>
2026-07-15 20:13:17 -0300

    Created folder scripts and main.js file. JS code relocated from .html to main.js. Added link to JS file and defer attribute in script src tag in order to wait for html to load and then execute JS

---
+++ src/interface/scripts/main.js
@@ -0,0 +1,38 @@
+  // ================= JS PARA ANIMACIONES ================= 
+
+    // Navbar cambia al hacer scroll
+       window.addEventListener("scroll", () => {
+
+            const header = document.getElementById("header");
+            const hero = document.getElementById("inicio");
+
+            const limite = hero.offsetHeight - 100;
+
+            header.classList.toggle("scrolled", window.scrollY > limite);
+        });
+
+        // Animación al hacer scroll
+const faders = document.querySelectorAll(".fade-up");
+
+    const appearOnScroll = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+
+            if (entry.isIntersecting) {
+
+                entry.target.classList.add("appear");
+
+            } else {
+                        entry.target.classList.remove("appear");
+                    }
+
+        });
+
+    }, {
+        threshold: 0.2
+    });
+
+    faders.forEach(fader => {
+
+        appearOnScroll.observe(fader);
+    });
+
